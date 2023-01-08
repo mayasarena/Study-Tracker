385e12cd5f5985d2238f78269fe7e77804ad67f1
Maya Murad <mmurad7@uwo.ca>
2023-01-08 00:24:07 -0500

    Initial Commit

---
+++ Study Tracker/Study_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study_TrackerApp.swift
+//  Study Tracker
+//
+//  Created by Maya Murad on 2023-01-08.
+//
+
+import SwiftUI
+
+@main
+struct Study_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
